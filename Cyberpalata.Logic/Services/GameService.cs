6d4355062d1a4d5ff40f0b837ad043704c7c11a1
Subaru <tikhon.grek@mail.ru>
2023-01-18 18:29:46 +0300

    GameService refactoring

--- Cyberpalata.Logic/Services/GameService.cs
+++ Cyberpalata.Logic/Services/GameService.cs
@@ -31,25 +31,31 @@ namespace Cyberpalata.Logic.Services
             await _repository.CreateAsync(_mapper.Map<Game>(entity));
         }
 
-        public async Task<GameDto> ReadAsync(Guid id)
+        public async Task<Maybe<GameDto>> ReadAsync(Guid id)
         {
-            return _mapper.Map<GameDto>(await _repository.ReadAsync(id));
+            var game = await _repository.ReadAsync(id);
+            return _mapper.Map<Maybe<GameDto>>(game);
         }
 
         public async Task<Result> DeleteAsync(Guid id)
         {
             var res = await SearchAsync(id);
+
             if (res.IsFailure)
                 return Result.Fail(res.Error);
+
             _repository.Delete(_mapper.Map<Game>(res.Value));
+
             return Result.Ok();
         }
-
+        //??? Do i need it
         public async Task<Result<GameDto>> SearchAsync(Guid id)
         {
             var game = await _repository.ReadAsync(id);
+
             if (!game.HasValue)
-                return (Result<GameDto>)Result.Fail($"Game with id {id} doesn't exist");
+                return Result.Fail<GameDto>($"Game with id {id} doesn't exist");
+
             return Result.Ok(_mapper.Map<GameDto>(game.Value));
         }
 
