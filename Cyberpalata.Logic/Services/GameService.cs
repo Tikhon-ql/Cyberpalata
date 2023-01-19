76a804c5963eca5327f8cc51b791b13eb2aff101
Subaru <tikhon.grek@mail.ru>
2023-01-19 18:30:04 +0300

    GameService refactoring

--- Cyberpalata.Logic/Services/GameService.cs
+++ Cyberpalata.Logic/Services/GameService.cs
@@ -1,10 +1,10 @@
 ﻿using AutoMapper;
+using CSharpFunctionalExtensions;
 using Cyberpalata.Common;
 using Cyberpalata.DataProvider.Interfaces;
 using Cyberpalata.DataProvider.Models;
 using Cyberpalata.Logic.Interfaces;
 using Cyberpalata.Logic.Models;
-using Functional.Maybe;
 
 namespace Cyberpalata.Logic.Services
 {
@@ -23,9 +23,9 @@ namespace Cyberpalata.Logic.Services
         public async Task<Result> CreateAsync(Maybe<GameDto> entity)
         {
             if (!entity.HasValue)
-                return Result.Fail("Invalid game creation request!");
+                return Result.Failure("Invalid game creation request!");
             await _repository.CreateAsync(_mapper.Map<Game>(entity));
-            return Result.Ok();
+            return Result.Success();
         }
 
         public async Task<Maybe<GameDto>> ReadAsync(Guid id)
@@ -39,11 +39,11 @@ namespace Cyberpalata.Logic.Services
             var res = await SearchAsync(id);
 
             if (res.IsFailure)
-                return Result.Fail(res.Error);
+                return Result.Failure(res.Error);
 
             _repository.Delete(_mapper.Map<Game>(res.Value));
 
-            return Result.Ok();
+            return Result.Success();
         }
         //??? Do i need it
         public async Task<Result<GameDto>> SearchAsync(Guid id)
@@ -51,9 +51,9 @@ namespace Cyberpalata.Logic.Services
             var game = await _repository.ReadAsync(id);
 
             if (!game.HasValue)
-                return Result.Fail<GameDto>($"Game with id {id} doesn't exist");
+                return Result.Failure<GameDto>($"Game with id {id} doesn't exist");
 
-            return Result.Ok(_mapper.Map<GameDto>(game.Value));
+            return Result.Success(_mapper.Map<GameDto>(game.Value));
         }
 
         public async Task<PagedList<GameDto>> GetPagedListAsync(int pageNumber)
