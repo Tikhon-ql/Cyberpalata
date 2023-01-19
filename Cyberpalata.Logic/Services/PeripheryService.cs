5134b62b21846344037bead5d3daac14fb2244b6
Subaru <tikhon.grek@mail.ru>
2023-01-19 18:32:59 +0300

    PeripheryService refactoring

--- Cyberpalata.Logic/Services/PeripheryService.cs
+++ Cyberpalata.Logic/Services/PeripheryService.cs
@@ -1,10 +1,10 @@
 ﻿using AutoMapper;
+using CSharpFunctionalExtensions;
 using Cyberpalata.Common;
 using Cyberpalata.DataProvider.Interfaces;
 using Cyberpalata.DataProvider.Models.Peripheral;
 using Cyberpalata.Logic.Interfaces;
 using Cyberpalata.Logic.Models.Peripheral;
-using Functional.Maybe;
 
 namespace Cyberpalata.Logic.Services
 {
@@ -23,9 +23,9 @@ namespace Cyberpalata.Logic.Services
         public async Task<Result> CreateAsync(Maybe<PeripheryDto> entity)
         {
             if (!entity.HasValue)
-                return Result.Fail("Invalid entity creation request!");
+                return Result.Failure("Invalid entity creation request!");
             await _repository.CreateAsync(_mapper.Map<Periphery>(entity));
-            return Result.Ok();
+            return Result.Success();
         }
 
         public async Task<Maybe<PeripheryDto>> ReadAsync(Guid id)
@@ -40,11 +40,11 @@ namespace Cyberpalata.Logic.Services
             var res = await SearchAsync(id);
 
             if (res.IsFailure)
-                return Result.Fail(res.Error);
+                return Result.Failure(res.Error);
 
             _repository.Delete(_mapper.Map<Periphery>(res.Value));
 
-            return Result.Ok();
+            return Result.Success();
         }
 
         public async Task<Result<PeripheryDto>> SearchAsync(Guid id)
@@ -52,9 +52,9 @@ namespace Cyberpalata.Logic.Services
             var periphery = await _repository.ReadAsync(id);
 
             if (!periphery.HasValue)
-                return Result.Fail<PeripheryDto>($"Periphery with id {id} doesn't exist");
+                return Result.Failure<PeripheryDto>($"Periphery with id {id} doesn't exist");
 
-            return Result.Ok(_mapper.Map<PeripheryDto>(periphery.Value));
+            return Result.Success(_mapper.Map<PeripheryDto>(periphery.Value));
         }
 
         public async Task<PagedList<PeripheryDto>> GetPagedListAsync(int pageNumber)
