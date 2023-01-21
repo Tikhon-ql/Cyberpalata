37a7a9c89ee2998c6e447e57103fc47a58076fbe
Subaru <tikhon.grek@mail.ru>
2023-01-22 00:55:09 +0300

    Booking orm model fullfillng by properties

--- Cyberpalata.DataProvider/Models/Booking.cs
+++ Cyberpalata.DataProvider/Models/Booking.cs
@@ -1,14 +1,22 @@
-﻿using Cyberpalata.DataProvider.Models.Rooms;
+﻿using Cyberpalata.DataProvider.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cyberpalata.DataProvider.Models
 {
     public class Booking
     {
+        [Key]
         public Guid Id { get; set; }
-        //public Room? Room { get; set; }
+        [Required]//constraint gaming room X game console room only
+        public Room Room { get; set; }
+        [Required]
         public DateTime Begining { get; set; }
+        [Required]
         public DateTime Ending { get; set; }
-        public Price? Tariff { get; set; }
-
+        [Required]
+        public Price Tariff { get; set; }
+        [Required]
+        public virtual List<Seat> Seats { get; set; }
+        public virtual List<Game> GamesToDownloadBefore { get; set; }//have to check is the game in the cyberclub games list
     }
 }
