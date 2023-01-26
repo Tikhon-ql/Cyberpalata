3dc3fb63d782a8c7b9bdda544450fe5cc1d165a1
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-01-26 21:18:48 +0300

    Seats adding into gamingRoomViewModel

--- Cyberpalata.ViewModel/Rooms/GamingRoom/GamingRoomViewModel.cs
+++ Cyberpalata.ViewModel/Rooms/GamingRoom/GamingRoomViewModel.cs
@@ -5,5 +5,6 @@
         public List<PcInfo> PcInfos { get; set; }
         public List<Periphery> Peripheries { get; set; } = new();
         public List<Price> Prices { get; set; } = new();
+        public List<int> Seats { get; set; } = new();
     }
 }
