79a8760627bc7fd0834886e943f02e65a8f8b670
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-01-26 21:22:16 +0300

    Room seat viewModel adding

---
+++ Cyberpalata.ViewModel/Rooms/Seat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyberpalata.ViewModel.Rooms
+{
+    public class Seat
+    {
+        public int Number { get; set; }
+        public bool IsFree { get; set; }
+    }
+}
