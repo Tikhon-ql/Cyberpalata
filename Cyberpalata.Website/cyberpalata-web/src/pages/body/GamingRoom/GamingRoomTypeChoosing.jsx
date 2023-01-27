1d4d308cd81071a6955cbea5bed4a4fc8be1c9f7
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-01-27 16:08:23 +0300

    Gaming room type choosing component editing

--- Cyberpalata.Website/cyberpalata-web/src/pages/body/GamingRoom/GamingRoomTypeChoosing.jsx
+++ Cyberpalata.Website/cyberpalata-web/src/pages/body/GamingRoom/GamingRoomTypeChoosing.jsx
@@ -1,29 +1,17 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { formToJSON } from "axios";
+import {Link} from "react-router-dom"
 
 export const GamingRoomTypeChoosing = ()=> {
   return (
-    <div
-      className="modal show"
-      style={{ display: 'block', position: 'initial' }}
-    >
-      <Modal.Dialog>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <p>Modal body text goes here.</p>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button variant="secondary">Close</Button>
-          <Button variant="primary">Save changes</Button>
-        </Modal.Footer>
-      </Modal.Dialog>
+    <div className="pt-5" style={{"height":"100vh","width":"100vw","display":"flex","alignItems":"center","justifyContent":"center"}}>
+    <div>
+    <h2>What type of gaming room you want to?</h2>
+    <div style={{'display': 'flex'}}>
+        <Link to="/gamingRooms/vip" className="btn btn-outline-dark w-25 m-5">Vip</Link>
+        <Link to="/gamingRooms/common" className="btn btn-outline-dark ml-3 w-25 h-25 m-5">Common</Link>
     </div>
+    </div>
+</div>
   );
 }
-
-
 // 
