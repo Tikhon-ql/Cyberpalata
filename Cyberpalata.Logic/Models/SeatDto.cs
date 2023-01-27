daa39597ee81d19b639e279448d9010ed2d10a7f
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-01-27 20:53:42 +0300

    SeatDto refactoring

--- Cyberpalata.Logic/Models/SeatDto.cs
+++ Cyberpalata.Logic/Models/SeatDto.cs
@@ -7,6 +7,5 @@ namespace Cyberpalata.Logic.Models
     {
         public Guid Id { get; set; }
         [Required] public int Number { get; set; }
-        [Required] public RoomDto Room { get; set; }
     }
 }
