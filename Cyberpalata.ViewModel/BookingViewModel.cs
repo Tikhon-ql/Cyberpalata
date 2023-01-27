31e95832aab3199f667e09500ca273fc3989a32e
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-01-27 16:08:42 +0300

    Booking view model adding

---
+++ Cyberpalata.ViewModel/BookingViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Cyberpalata.ViewModel.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyberpalata.ViewModel
+{
+    public class BookingViewModel
+    {
+        public string RoomName { get; set; }
+        public List<int> Seats { get; set; } = new();
+        public List<Price> Tariffs { get; set; } = new();
+    }
+}
