2e10f74f8e01034c12b1fa0a85d7ce800073f9ab
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-01-27 20:52:50 +0300

    Mapping configuration edititng

--- Cyberpalata.Logic/Configuration/AppMappingProfile.cs
+++ Cyberpalata.Logic/Configuration/AppMappingProfile.cs
@@ -9,6 +9,8 @@ using Cyberpalata.Common;
 using Cyberpalata.DataProvider.Models.Identity;
 using Cyberpalata.Logic.Models.Identity;
 using CSharpFunctionalExtensions;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Cyberpalata.Common.Enums;
 
 namespace Cyberpalata.Logic.Configuration
 {
@@ -17,23 +19,25 @@ namespace Cyberpalata.Logic.Configuration
         public AppMappingProfile()
         {
 
-
             CreateMap<Game, GameDto>();
             CreateMap<PagedList<Game>, PagedList<GameDto>>();
             CreateMap<Maybe<Game>, Maybe<GameDto>>();
             CreateMap<Maybe<List<Game>>, Maybe<List<GameDto>>>();
-    
+
             CreateMap<Price, PriceDto>();
-            CreateMap<PagedList<Price>, PagedList<PriceDto>>();
-            CreateMap<Maybe<Price>, Maybe<PriceDto>>();
-            CreateMap<Maybe<List<Price>>, Maybe<List<PriceDto>>>();
+            CreateMap<PriceDto, Price>();
+            //CreateMap<PagedList<Price>, PagedList<PriceDto>>();
+            //CreateMap<Maybe<Price>, Maybe<PriceDto>>();
+            //CreateMap<Maybe<List<Price>>, Maybe<List<PriceDto>>>();
 
             CreateMap<Seat, SeatDto>();
-            CreateMap<PagedList<Seat>, PagedList<SeatDto>>();
-            CreateMap<Maybe<Seat>, Maybe<SeatDto>>();
-            CreateMap<Maybe<List<Seat>>, Maybe<List<SeatDto>>>();
+            CreateMap<SeatDto, Seat>();
+            //CreateMap<PagedList<Seat>, PagedList<SeatDto>>();
+            //CreateMap<Maybe<Seat>, Maybe<SeatDto>>();
+            //CreateMap<Maybe<List<Seat>>, Maybe<List<SeatDto>>>();
 
             CreateMap<MenuItem, MenuItemDto>();
+            CreateMap<MenuItemDto, MenuItem>();
             CreateMap<PagedList<MenuItem>, PagedList<MenuItemDto>>();
             CreateMap<Maybe<MenuItem>, Maybe<MenuItemDto>>();
             CreateMap<Maybe<List<MenuItem>>, Maybe<List<MenuItemDto>>>();
@@ -64,27 +68,21 @@ namespace Cyberpalata.Logic.Configuration
                 .ForMember(dst => dst.Password, opt => opt.MapFrom(src => src.Password));
 
             CreateMap<ApiUser, ApiUserDto>();
-            CreateMap<Maybe<ApiUser>,Maybe<ApiUserDto>>();
+            CreateMap<Maybe<ApiUser>, Maybe<ApiUserDto>>();
 
             CreateMap<Booking, BookingDto>();
-            CreateMap<PagedList<Booking>,PagedList<BookingDto>>();
+            CreateMap<BookingDto, Booking>();
+            //CreateMap<Maybe<Booking>, Maybe<BookingDto>>();
+            //CreateMap<PagedList<Booking>,PagedList<BookingDto>>();
 
-            CreateMap<Room, RoomDto>()
-                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dst => dst.Name, opt => opt.MapFrom(src => src.Name))
-                .ForMember(dst => dst.Bookings, opt => opt.MapFrom(src => src.Bookings))
-                .ForMember(dst => dst.Seats, opt => opt.MapFrom(src => src.Seats))
-                .ForMember(dst => dst.Prices, opt => opt.MapFrom(src => src.Prices))
-                .ForMember(dst => dst.IsVip, opt => opt.MapFrom(src => src.IsVip))
-                .ForMember(dst => dst.Type, opt => opt.MapFrom(src => src.Type));
-            CreateMap<Maybe<Room>, Maybe<RoomDto>>()
-                .ForMember(dst=>dst.Value, opt=> opt.MapFrom(src=>src.Value));
-            CreateMap<PagedList<Room>, PagedList<RoomDto>>()
-                .ForMember(dst => dst.Items, opt => opt.MapFrom(src => src.Items))
-                .ForMember(dst => dst.CurrentPageNumber, opt => opt.MapFrom(src => src.CurrentPageNumber))
-                .ForMember(dst => dst.PageSize, opt => opt.MapFrom(src => src.PageSize))
-                .ForMember(dst => dst.TotalItemsCount, opt => opt.MapFrom(src => src.TotalItemsCount))
-                .ForMember(dst => dst.TotalPagesCount, opt => opt.MapFrom(src => src.TotalPagesCount));
+            CreateMap<Room, RoomDto>();
+            CreateMap<PagedList<Room>, PagedList<RoomDto>>();
+            CreateMap<RoomDto, Room>();
+            CreateMap<Maybe<Room>, Maybe<RoomDto>>();
+            CreateMap<PagedList<Room>, PagedList<RoomDto>>();
+            CreateMap<PagedList<RoomDto>, PagedList<Room>>();
+
+
             //CreateMap<Maybe<List<Room>>, Maybe<List<RoomDto>>>();
         }
     }
