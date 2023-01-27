87734706f61c6382a3413203f6883a94928895ac
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-01-27 16:33:48 +0300

    Maping configuration editing

--- Cyberpalata.Logic/Configuration/AppMappingProfile.cs
+++ Cyberpalata.Logic/Configuration/AppMappingProfile.cs
@@ -69,10 +69,23 @@ namespace Cyberpalata.Logic.Configuration
             CreateMap<Booking, BookingDto>();
             CreateMap<PagedList<Booking>,PagedList<BookingDto>>();
 
-            CreateMap<Room, RoomDto>();
-            CreateMap<PagedList<Room>, PagedList<RoomDto>>();
-            CreateMap<Maybe<Room>, Maybe<RoomDto>>();
-            CreateMap<Maybe<List<Room>>, Maybe<List<RoomDto>>>();
+            CreateMap<Room, RoomDto>()
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dst => dst.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dst => dst.Bookings, opt => opt.MapFrom(src => src.Bookings))
+                .ForMember(dst => dst.Seats, opt => opt.MapFrom(src => src.Seats))
+                .ForMember(dst => dst.Prices, opt => opt.MapFrom(src => src.Prices))
+                .ForMember(dst => dst.IsVip, opt => opt.MapFrom(src => src.IsVip))
+                .ForMember(dst => dst.Type, opt => opt.MapFrom(src => src.Type));
+            CreateMap<Maybe<Room>, Maybe<RoomDto>>()
+                .ForMember(dst=>dst.Value, opt=> opt.MapFrom(src=>src.Value));
+            CreateMap<PagedList<Room>, PagedList<RoomDto>>()
+                .ForMember(dst => dst.Items, opt => opt.MapFrom(src => src.Items))
+                .ForMember(dst => dst.CurrentPageNumber, opt => opt.MapFrom(src => src.CurrentPageNumber))
+                .ForMember(dst => dst.PageSize, opt => opt.MapFrom(src => src.PageSize))
+                .ForMember(dst => dst.TotalItemsCount, opt => opt.MapFrom(src => src.TotalItemsCount))
+                .ForMember(dst => dst.TotalPagesCount, opt => opt.MapFrom(src => src.TotalPagesCount));
+            //CreateMap<Maybe<List<Room>>, Maybe<List<RoomDto>>>();
         }
     }
 }
