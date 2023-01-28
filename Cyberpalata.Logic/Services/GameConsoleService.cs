59116896456ea54d037aa2a30383355fe82f6cd2
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-01-28 20:57:32 +0300

    Getting gameconsoles by room

--- Cyberpalata.Logic/Services/GameConsoleService.cs
+++ Cyberpalata.Logic/Services/GameConsoleService.cs
@@ -11,11 +11,13 @@ namespace Cyberpalata.Logic.Services
     internal class GameConsoleService : IGameConsoleService
     {
         private readonly IGameConsoleRepository _repository;
+        private readonly IRoomRepository _roomRepository;
         private readonly IMapper _mapper;
-        public GameConsoleService(IGameConsoleRepository repository, IMapper mapper)
+        public GameConsoleService(IGameConsoleRepository repository,IRoomRepository roomRepository, IMapper mapper)
         {
             _repository = repository;
             _mapper = mapper;
+            _roomRepository = roomRepository;
         }
 
         public async Task CreateAsync(GameConsoleDto entity)
@@ -57,7 +59,10 @@ namespace Cyberpalata.Logic.Services
 
         public async Task<Maybe<List<GameConsoleDto>>> GetByGameConsoleRoomId(Guid roomId)
         {
-            var list = await _repository.GetByGameConsoleRoomIdAsync(roomId);
+            var room = await _roomRepository.ReadAsync(roomId);
+            if (room.HasNoValue)
+                return Maybe.None;
+            var list = room.Value.Consoles;
             return _mapper.Map<Maybe<List<GameConsoleDto>>>(list);
         }
     }
