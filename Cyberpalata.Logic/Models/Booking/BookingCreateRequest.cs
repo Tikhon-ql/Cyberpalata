1a9feae4c480b87d919349f5ac3109fd68408dc0
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-01-29 19:13:46 +0300

    Booking create request adding

---
+++ Cyberpalata.Logic/Models/Booking/BookingCreateRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyberpalata.Logic.Models.Booking
+{
+    public class BookingCreateRequest
+    {
+        public DateTime Begining { get; set; }
+        public DateTime Ending { get; set; }
+        public string Tariff { get; set; }
+        public List<int> Seats { get; set; } = new();
+    }
+}
