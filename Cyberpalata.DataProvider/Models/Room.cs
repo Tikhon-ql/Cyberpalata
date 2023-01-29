ed77ae3ba02674632ffe642849e55ad25db653bf
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-01-29 19:07:07 +0300

    Room model refactoring

--- Cyberpalata.DataProvider/Models/Room.cs
+++ Cyberpalata.DataProvider/Models/Room.cs
@@ -7,12 +7,16 @@ namespace Cyberpalata.DataProvider.Models
 {
     public class Room
     {
-        [Key] public Guid Id { get; set; }
-        [Required][MaxLength(50)] public string Name { get; set; }
-        [Required] public RoomType Type { get; set; }
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        public virtual RoomType Type { get; set; }
         public bool IsVip { get; set; }
         public virtual List<Price> Prices { get; set; }
-        public virtual List<Seat> Seats { get; set; } 
+        public virtual List<Seat> Seats { get; set; }
         public virtual List<Booking> Bookings { get; set; }
         public virtual List<GameConsole> Consoles { get; set; }
         public virtual List<Pc> Pcs { get; set; }
