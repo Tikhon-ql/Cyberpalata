8a252902aa6b58fb18fd6a17ce4651623f1747ac
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-01-29 19:06:47 +0300

    Price model refactoring

--- Cyberpalata.DataProvider/Models/Price.cs
+++ Cyberpalata.DataProvider/Models/Price.cs
@@ -4,9 +4,13 @@ namespace Cyberpalata.DataProvider.Models
 {
     public class Price
     {
-        [Key] [Required] public Guid Id { get; set; }
-        [Required] public int Hours { get; set; }
-        [Required] public decimal Cost { get; set; }
-        public Room Room { get; set; }
+        [Key] 
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public int Hours { get; set; }
+        [Required]
+        public decimal Cost { get; set; }
+        public virtual Room Room { get; set; }
     }
 }
