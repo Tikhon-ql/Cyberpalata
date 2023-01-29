aaedd18c5de46b5ebec7b646bfbafb864e1aa322
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-01-29 11:34:14 +0300

    Game edititng

--- Cyberpalata.DataProvider/Models/Game.cs
+++ Cyberpalata.DataProvider/Models/Game.cs
@@ -4,7 +4,11 @@ namespace Cyberpalata.DataProvider.Models
 {
     public class Game
     {
-        [Key] [Required] public Guid Id { get; set; }
-        [MaxLength(50)] [Required] public string GameName { get; set; }
+        [Key] 
+        [Required] 
+        public Guid Id { get; set; }
+        [MaxLength(50)] 
+        [Required] 
+        public string GameName { get; set; }
     }
 }
