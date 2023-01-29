885bc476383a3f18349a92944cbf451c392788c7
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-01-29 19:07:41 +0300

    Pc model refactoring

--- Cyberpalata.DataProvider/Models/Devices/PC.cs
+++ Cyberpalata.DataProvider/Models/Devices/PC.cs
@@ -4,12 +4,24 @@ namespace Cyberpalata.DataProvider.Models.Devices
 {
     public class Pc
     {
-        [Key][Required] public Guid Id { get; set; }
-        [Required][MaxLength(50)] public string Gpu { get; set; }
-        [Required][MaxLength(50)] public string Cpu { get; set; }
-        [Required][MaxLength(50)] public string Ram { get; set; }
-        [Required][MaxLength(50)] public string Hdd { get; set; }
-        [Required][MaxLength(50)] public string Ssd { get; set; }
+        [Key]
+        [Required] 
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(50)] 
+        public string Gpu { get; set; }
+        [Required]
+        [MaxLength(50)] 
+        public string Cpu { get; set; }
+        [Required]
+        [MaxLength(50)] 
+        public string Ram { get; set; }
+        [Required]
+        [MaxLength(50)] 
+        public string Hdd { get; set; }
+        [Required]
+        [MaxLength(50)] 
+        public string Ssd { get; set; }
         public virtual Room GamingRoom { get; set; }
     }
 }
