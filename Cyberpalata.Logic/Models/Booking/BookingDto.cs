f4e10ae36417c5782c93fc093a74e373bc185336
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-01-29 19:14:53 +0300

    BookingDto moving into booking folder

---
+++ Cyberpalata.Logic/Models/Booking/BookingDto.cs
@@ -0,0 +1,27 @@
+﻿using Cyberpalata.DataProvider.Models;
+using Cyberpalata.Logic.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyberpalata.Logic.Models.Booking
+{
+    public class BookingDto
+    {
+        [Required]
+        public ApiUserDto User { get; set; }
+        [Required]
+        public DateTime Begining { get; set; }
+        [Required]
+        public DateTime Ending { get; set; }
+        [Required]
+        public PriceDto Tariff { get; set; }
+        [Required]
+        public List<SeatDto> Seats { get; set; }
+        public List<GameDto> GamesToDownloadBefore { get; set; } = new();//have to check is the game in the cyberclub games list
+        public bool IsExpired { get; set; } // trigger
+    }
+}
