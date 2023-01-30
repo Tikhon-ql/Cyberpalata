f67867383d9b70fa848bf28a6d71219e3279bf35
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-01-30 15:54:14 +0300

    Booking model editing

--- Cyberpalata.DataProvider/Models/Booking.cs
+++ Cyberpalata.DataProvider/Models/Booking.cs
@@ -1,6 +1,7 @@
 ﻿using Cyberpalata.DataProvider.Models;
 using Cyberpalata.DataProvider.Models.Identity;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Cyberpalata.DataProvider.Models
 {
