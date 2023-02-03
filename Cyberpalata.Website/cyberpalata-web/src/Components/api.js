f32d7380b077060aa600cfb324445aaddf073ac8
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-02-03 21:36:04 +0300

    Api component to send requests to api

--- Cyberpalata.Website/cyberpalata-web/src/Components/api.js
+++ Cyberpalata.Website/cyberpalata-web/src/Components/api.js
@@ -13,7 +13,41 @@ api.interceptors.request.use(
     error=>{
         if(error.response.status === 401)
         {
-            console.log(401);
+            const accessToken = localStorage.getItem('accessToken');
+            if(accessToken != null)
+            {
+                //const decodedToken = jwtDecode(accessToken);
+                //console.log(decodedToken);
+                //console.log((Date.now() / 1000));
+                //let minutes = 1 * 30;
+                // if(decodedToken.exp - minutes < (Date.now() / 1000))
+                // {
+                //console.log(decodedToken.exp - minutes);
+                //console.log(Date.now() / 1000);
+                var refreshToken = localStorage.getItem('refreshToken');
+                if(refreshToken != null)
+                {
+                    console.log("Access token has been refreshed.");
+                    //const apiRequestUrl = `https://localhost:7227/authentication/refresh`;
+                    // const config = {
+                    //     headers: { Authorization: `Bearer ${accessToken}` }
+                    // };
+                    const requestBody =
+                    {       
+                        "accessToken" : accessToken,
+                        "refreshToken":refreshToken
+                    }
+                    api.post(`/authentication/refresh`,requestBody).then(res=>
+                    {
+                        localStorage.setItem("accessToken", res.data.accessToken);
+                        localStorage.setItem("refreshToken", res.data.refreshToken);
+                    }).catch(console.log);
+                }
+                else{
+                    alert("There are some trubles: refresh token is undefined.");
+                }
+                //}
+            }
         }
     }
 )
