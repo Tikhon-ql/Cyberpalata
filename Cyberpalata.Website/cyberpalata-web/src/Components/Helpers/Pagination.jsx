95cb7cb5910c2b4f42a15dd3ed429efc54c7ef76
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-02-04 21:26:36 +0300

    Pagination component

--- Cyberpalata.Website/cyberpalata-web/src/Components/Helpers/Pagination.jsx
+++ Cyberpalata.Website/cyberpalata-web/src/Components/Helpers/Pagination.jsx
@@ -1,5 +1,16 @@
-export const Pagination = ({pageCount ,setCurPage})=>{
-
-    return <>
-        </>
-}
+export const Pagination = ({pageCount, curPage,setCurPage,totalItemsCount}) =>
+{
+    let pages = [];
+    for(let i = 1; i <= Math.ceil(totalItemsCount / pageCount);i++)
+    {
+        pages.push(i);
+    }
+    console.dir(pages);
+    return(
+        <ul class="pagination">
+            {pages.map((page,index) => {
+                return <li key={index} onClick={()=>{console.dir(page) ;setCurPage(page);}} className = {page == curPage ? 'page-item active' : 'page-item'}><a className="page-link">{page}</a></li>
+            })}
+        </ul>
+    )
+};
