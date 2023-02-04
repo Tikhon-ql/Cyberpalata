996f265968e607d0bc1b1226957d218331e41d9f
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-02-04 14:54:42 +0300

    Commit

---
+++ Cyberpalata.Website/cyberpalata-web/src/Components/Errors/500/IternalServerError.jsx
@@ -0,0 +1,9 @@
+
+export const IternalServerError = () => {
+    return <div className="flex h-screen">
+        <div className="m-auto">
+            <h1 className="text-center">500</h1>
+            <h2 className="text-center">Unexpected Error <b>:(</b></h2>
+        </div>
+    </div>
+}
---
+++ Cyberpalata.Website/cyberpalata-web/src/Components/Helpers/Pagination.jsx
@@ -0,0 +1,5 @@
+export const Pagination = ({pageCount ,setCurPage})=>{
+
+    return <>
+        </>
+}
