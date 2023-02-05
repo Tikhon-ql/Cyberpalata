a51700bb64908cf62f02fa37812774ea131fd852
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-02-05 21:02:12 +0300

    AccessTokenVerifty returning

--- Cyberpalata.Website/cyberpalata-web/src/Components/AccessTokenVerify.jsx
+++ Cyberpalata.Website/cyberpalata-web/src/Components/AccessTokenVerify.jsx
@@ -1,55 +1,51 @@
-// import { useLocation } from "react-router-dom";
-// import { useEffect } from "react";
-// import jwtDecode from "jwt-decode";
-// import api from "./../Components/api";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import jwtDecode from "jwt-decode";
+import api from "./../Components/api";
 
-// export const AccessTokenVerify = (props) => {
-//     //let location = useLocation();
+export const AccessTokenVerify = (props) => {
+    let location = useLocation();
 
-//     function checkAccessTokenExpirationTime()
-//     {
-//         const accessToken = localStorage.getItem('accessToken');
-//         if(accessToken != null)
-//         {
-//             const decodedToken = jwtDecode(accessToken);
-//             //console.log(decodedToken);
-//             //console.log((Date.now() / 1000));
-//             let minutes = 1 * 30;
-//             if(decodedToken.exp - minutes < (Date.now() / 1000))
-//             {
-//                 console.log(decodedToken.exp - minutes);
-//                 console.log(Date.now() / 1000);
-//                 var refreshToken = localStorage.getItem('refreshToken');
-//                 if(refreshToken != null)
-//                 {
-//                     console.log("Access token has been refreshed.");
-//                     //const apiRequestUrl = `https://localhost:7227/authentication/refresh`;
-//                     // const config = {
-//                     //     headers: { Authorization: `Bearer ${accessToken}` }
-//                     // };
-//                     const requestBody =
-//                     {       
-//                         "accessToken" : accessToken,
-//                         "refreshToken":refreshToken
-//                     }
-//                     var res = api.post(`/authentication/refresh`,requestBody).then(res=>
-//                     {
-//                         localStorage.setItem("accessToken", res.data.accessToken);
-//                         localStorage.setItem("refreshToken", res.data.refreshToken);
-//                     }).catch(console.log);
-//                 }
-//                 else{
-//                     alert("There are some trubles: refresh token is undefined.");
-//                 }
-//             }
-//         }
-//     }
+    function checkAccessTokenExpirationTime()
+    {
+        const accessToken = localStorage.getItem('accessToken');
+        if(accessToken != null)
+        {
+            const decodedToken = jwtDecode(accessToken);
+            //console.log(decodedToken);
+            //console.log((Date.now() / 1000));
+            let minutes = 1 * 30;
+            if(decodedToken.exp - minutes < (Date.now() / 1000))
+            {
+                console.log(decodedToken.exp - minutes);
+                console.log(Date.now() / 1000);
+                var refreshToken = localStorage.getItem('refreshToken');
+                if(refreshToken != null)
+                {
+                    const requestBody =
+                    {       
+                        "accessToken" : accessToken,
+                        "refreshToken":refreshToken
+                    }
+                    api.post(`/authentication/refresh`,requestBody).then(res=>
+                        {
+                            localStorage.setItem("accessToken", res.data.accessToken);
+                            localStorage.setItem("refreshToken", res.data.refreshToken);
+                        }).catch(console.log);      
+                    console.log("Access token has been refreshed.");
+                }
+                else{
+                    alert("There are some trubles: refresh token is undefined.");
+                }        
+            }
+        }
+    }
 
 
-//     // useEffect(()=>{
-//     //     setInterval(checkAccessTokenExpirationTime,10000);
-//         //console.log(location);
+    useEffect(()=>{
+        setInterval(checkAccessTokenExpirationTime,10000);
+        console.log(location);
        
-//     // },[]);//[location, props]);
-//     return <></>;
-// }
+    },[]);//[location, props]);
+    return <></>;
+}
