e30532de8c3c26c3cf3a04a37e1db511d91e8387
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-02-05 20:58:30 +0300

    New seats getting

--- Cyberpalata.Logic/Interfaces/Services/ISeatService.cs
+++ Cyberpalata.Logic/Interfaces/Services/ISeatService.cs
@@ -1,11 +1,12 @@
 ﻿using CSharpFunctionalExtensions;
-using Cyberpalata.Logic.Models;
+using Cyberpalata.Logic.Models.Seats;
 
 namespace Cyberpalata.Logic.Interfaces.Services
 {
     public interface ISeatService : IService<SeatDto>
     {
         Task CreateRangeAsync(List<SeatDto> seats);
-        Task<Maybe<List<SeatDto>>> GetSeatsByRoomId(Guid roomId);
+        Task<Maybe<List<SeatDto>>> GetSeatsByRoomIdAsync(Guid roomId);
+        Task<Maybe<List<SeatDto>>> GetSeatsByRoomInRangeIdAsync(SeatsGettingRequest request);
     }
 }
