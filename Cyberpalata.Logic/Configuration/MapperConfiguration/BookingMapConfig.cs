9f510903b70884cdc522332ff5f7a78b9d58634a
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-02-05 20:59:25 +0300

    Booking date mapping adding

--- Cyberpalata.Logic/Configuration/MapperConfiguration/BookingMapConfig.cs
+++ Cyberpalata.Logic/Configuration/MapperConfiguration/BookingMapConfig.cs
@@ -3,6 +3,7 @@ using Cyberpalata.Common;
 using Cyberpalata.DataProvider.Models;
 using Cyberpalata.Logic.Models;
 using Cyberpalata.Logic.Models.Booking;
+using Cyberpalata.Logic.Models.Seats;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,7 @@ namespace Cyberpalata.Logic.Configuration.MapperConfiguration
             profile.CreateMap<PagedList<Booking>, PagedList<BookingDto>>();
 
             profile.CreateMap<BookingCreateRequest, BookingDto>()
+                .ForMember(dst=>dst.Date, opt=>opt.MapFrom(src=>src.Date))
                 .ForMember(dst => dst.Begining, opt => opt.MapFrom(src => src.Begining))
                 .ForMember(dst => dst.Ending, opt => opt.MapFrom(src => src.Ending))
                 .ForMember(dst => dst.Tariff, opt => opt.MapFrom(src => src.Tariff))
