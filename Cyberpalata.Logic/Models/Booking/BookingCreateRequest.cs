b30385b20ff386ba660b204499f3966dff0246eb
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-02-08 02:53:46 +0300

    DataAnnotation booking validation adding

--- Cyberpalata.Logic/Models/Booking/BookingCreateRequest.cs
+++ Cyberpalata.Logic/Models/Booking/BookingCreateRequest.cs
@@ -1,7 +1,7 @@
 ﻿using Cyberpalata.Logic.Models.Identity.User;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +11,15 @@ namespace Cyberpalata.Logic.Models.Booking
     public class BookingCreateRequest
     {
         public Guid RoomId { get; set; }
+        [Required]
         public DateTime Date { get; set; }
+        [Required]
         public TimeSpan Begining { get; set; }
+        [Required]
         public int HoursCount { get; set; }
+        [Required]
         public decimal Price { get; set; }
+        [Required]
         public List<int> Seats { get; set; } = new();
     }
 }
