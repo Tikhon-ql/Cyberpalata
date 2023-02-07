f1773ad1f7082c26cb0be19e4e711146bab2de94
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-02-08 02:58:41 +0300

    Booking view compoent validation adding

--- Cyberpalata.Website/cyberpalata-web/src/pages/body/Booking/BookingComponent.jsx
+++ Cyberpalata.Website/cyberpalata-web/src/pages/body/Booking/BookingComponent.jsx
@@ -1,19 +1,24 @@
 import "./BookingComponent.css";
 import jwtDecode from "jwt-decode";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import api from "./../../../Components/api";
 import { servicesVersion } from "typescript";
 import BarLoader from "react-spinners/BarLoader";
 
 export const BookingComponent = () => {
     
+    const navigate = useNavigate();
     const {roomId} = useParams()
     const {roomName} = useParams();
     const [seats,setSeats] = useState([]);
     const [price, setPrice] = useState(0);
     const [clickedSeats, setClickedSeats] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [otherError,setOtherError] = useState("");
+    const [dateError, setDateError] = useState("");
+    const [priceError, setPriceError] = useState("");
+    const [hoursCountError,setHoursCountError] = useState("");
     const apiUrl = `https://localhost:7227;`;
 
     let accessToken = localStorage.getItem('accessToken');
@@ -22,6 +27,7 @@ export const BookingComponent = () => {
     function onTimeChange(event)
     {
         console.dir(event);
+        clearErrors();
         let date = document.forms[0].date.value;
         let beg = document.forms[0].begining.value;
         let hours = document.forms[0].hours.value;
@@ -39,21 +45,43 @@ export const BookingComponent = () => {
             }
             api.post(`/seats/getSeats`,requestBody).then(res=>{
                 setSeats(res.data);
+            }).catch(err=>{
+                if(err.response.status >= 500 && err.response.status <= 599)
+                {
+                    navigate("/500");
+                }
             })
             api.get(`/booking/getPrice?beg=${beg}&hours=${hours}`).then(res=>{
                 setPrice(res.data);
+            }).catch(err=>{
+                if(err.response.status >= 500 && err.response.status <= 599)
+                {
+                    navigate("/500");
+                }
             });
         }     
     }
 
+    function clearErrors()
+    {
+        setDateError("");
+        setOtherError("");
+        setPriceError("");
+        setHoursCountError("");
+    }
+
     useEffect(()=>{
         api.get(`/booking/seats?roomId=${roomId}`).then(res=>{
-
             //console.dir(res);
             //setSeats(res.data.seats);
             //setTarrifs(res.data.tariffs);
             //tarrifs.sort((a,b) => a.hours > b.hours);
-        }).catch(console.log);
+        }).catch(err=>{
+            if(err.status >= 500 && err.status <= 599)
+            {
+                navigate("/500");
+            }
+        });
     },[clickedSeats]);
   
     let columnCount = 10;
@@ -89,8 +117,31 @@ export const BookingComponent = () => {
             console.dir(requestBody);
             api.post(`/booking`, requestBody).then(res=>{
                 setClickedSeats([]);
-                setLoading(false);
-            }).catch(console.log);
+                setSeats([]);
+            }).catch((error)=>{
+                if(error.status >= 500 && error.status <= 599)
+                {
+                    navigate('/');
+                }
+                const data = error.response.data;
+                if(data.Other)
+                {
+                    setOtherError(data.Other);
+                }
+                if(data.Date)
+                {
+                    setDateError(data.Date);
+                }
+                if(data.Price)
+                {
+                    setPriceError(data.Price);
+                }
+                if(data.HoursCount)
+                {
+                    setHoursCountError(data.HoursCount);
+                }
+            })
+            .finally(()=>{ setLoading(false);});
         }
     }
 
@@ -110,7 +161,7 @@ export const BookingComponent = () => {
         }         
         console.dir(clickedSeats);
     }
-    return <div style={{"display":"flex","justifyContent":"center","alignItems":"center","width":"100%","height":"80vh"}}>
+    return <div style={{"display":"flex","justifyContent":"center","alignItems":"center","width":"100%","height":"80vh","marginTop":"5vh"}}>
         {loading ? 
       <div> 
         <BarLoader
@@ -122,20 +173,23 @@ export const BookingComponent = () => {
         :<div>
             <div className="d-flex align-items-center justify-content-center">
                 <form method="post" onSubmit={sendBookToServer} className="p-5 m-2 bg-info text-white shadow rounded-2" style={{"margin":"auto", "border" : "3px solid black", "padding" : "10px"}}>
+                    {otherError != "" && <div className="text-danger m-1 rounded">{otherError}</div>}
                     <h2 className="mx-auto">{roomName}</h2>
                     <h2>Dates</h2>
                     <div className="">
                         <label htmlFor="date" className="m-3">
                             <div>Date</div>
-                            <input id="date" name="data" onInput={onTimeChange} type="date"/>
+                            <input id="date" name="data" onChange={onTimeChange} type="date"/>
+                            {dateError != "" && <div className="text-danger m-1 rounded">{dateError}</div>}
                         </label>
                         <label htmlFor="begining" className="m-3">
                             <div>Begining</div>
-                            <input id="begining" name="begining" onInput={onTimeChange} type="time"/>
+                            <input id="begining" name="begining" onChange={onTimeChange} type="time"/>
                         </label>
                         <label htmlFor="hoursCount" className="m-3 w-100 ">
                             <div className="d-flex">Hours count(0-10):<div id="rangeVal" style={{"marginLeft":"5px"}}>10</div></div>
-                            <input id="hours" name="hours" className="w-50" onInput={onTimeChange} type="range" min = "1" max = "10"/>
+                            <input id="hours" name="hours" className="w-50" onChange={onTimeChange} type="range" min = "1" max = "10"/>
+                            {hoursCountError != "" && <div className="text-danger m-1 rounded">{hoursCountError}</div>}
                         </label>
                     </div>
                     {seats.length != 0 ?
@@ -154,21 +208,22 @@ export const BookingComponent = () => {
                                     })}
                                     </tbody>         
                                 </table>
+                                <div>
+                                {price != 0 ? 
+                                <div className="d-flex">
+                                    <h2 className="m-1">Your price: </h2>
+                                    <label id = "price" name = "price" class="label label-default h2 m-1">{price}</label>
+                                </div>:
+                                <div>
                             </div>
-                    : <h2>Enter correct data and time to view available seats</h2>
-                    }
-                    <div>
-                        {price != 0 ? 
-                        <div className="d-flex">
-                            <h2 className="m-1">Your price: </h2>
-                            <label id = "price" name = "price" class="label label-default h2 m-1">{price}</label>
-                        </div>:
-                        <div>
-
-                        </div>
                         }
                     <input type="submit" className="btn btn-dark btn-sm text-white w-25 m-1" style={{"marginTop":"1vh"}} value="Book"/>  
                     </div>
+                            </div>
+                    : <div><h2>Enter correct data and time to view available seats</h2>
+                    </div> 
+                    }
+                  
                     {/* <div>
                         <h2>Tarrifs</h2>
                             <div className="m-auto">
