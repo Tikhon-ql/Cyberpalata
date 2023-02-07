3a8fbafe0e7e60e072911e1705cf4ffd79aef15a
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-02-08 02:54:12 +0300

    DataAnnotation user validation adding

--- Cyberpalata.Logic/Models/Identity/User/UserUpdateRequest.cs
+++ Cyberpalata.Logic/Models/Identity/User/UserUpdateRequest.cs
@@ -10,10 +10,14 @@ namespace Cyberpalata.Logic.Models.Identity.User
     public class UserUpdateRequest
     {
         public Guid UserId { get; set; }
+        [Required]
         public string Username { get; set; } = String.Empty;
+        [Required]
         public string Surname { get; set; } = String.Empty;
+        [Required]
         [RegularExpression("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$")]
         public string Email { get; set; } = String.Empty;
+        [Required]
         [RegularExpression(pattern: "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$")]
         public string Phone { get; set; } = String.Empty;
     }
