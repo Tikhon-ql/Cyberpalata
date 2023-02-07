8f421c5904985c0d48a1a6b4fc5b2b9c02f24573
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-02-07 22:06:16 +0300

    Gaming rooms list view refactoring

--- Cyberpalata.Website/cyberpalata-web/src/pages/body/GamingRoom/GamingRoomList.jsx
+++ Cyberpalata.Website/cyberpalata-web/src/pages/body/GamingRoom/GamingRoomList.jsx
@@ -3,29 +3,47 @@ import { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom'
 import { useParams } from 'react-router-dom'
 import api from "./../../../Components/api";
+import BarLoader from "react-spinners/BarLoader";
 
 export const GamingRoomList = () =>{
 
     const [gamingRoomsInfo, setGamingRoomsInfo] = useState([]);
+    const [loading, setLoading] = useState(true);
     const {type} = useParams();
     useEffect(()=>{
-        api.get(`/gamingRooms/type?type=${type}`).then(res => {
-            setGamingRoomsInfo(res.data.infos);
-        })
+        setTimeout(()=>{
+            api.get(`/gamingRooms/type?type=${type}`).then(res => {
+                setGamingRoomsInfo(res.data.infos);
+            })
+            setLoading(false);
+        },1000);
     },[]); 
     //console.dir(gameConsoleRoomsInfo);
     // const sort = gameConsoleRoomsInfo.sort((a,b)=>{a.name < b.name ? 1 : -1});
     // console.dir(sort);
-    return (
-        <div className="mt-5 p-5" style={{"margin":"auto","width":"50%", "border" : "3px solid black", "padding" : "10px"}}>
-            <h1>Gaming rooms</h1>
-            <ul class="list-group list-group-flush">
-                {gamingRoomsInfo.map(item=>{
-                    return <li class="list-group-item" key={item.id}><Link to={`/gamingRooms/${item.id}/${item.name}/${type}`} className="text-decoration-none text-dark">{item.name}</Link></li>
-                })}
-            </ul>
-            <Link to='/' className="btn btn-outline-dark btn-sm mt-2">Home page</Link>
+    return <div style={{"display":"flex","justifyContent":"center","alignItems":"center","width":"100%","height":"80vh"}}>
+        {loading ? 
+        <div>
+              <BarLoader
+                color={"#123abc"}
+                loading={loading}
+                size={30}
+                />
         </div>
-        
-    )
+        : 
+        <div className='d-flex align-items-center justify-content-center'>
+            <div className="p-5 m-2 bg-info text-white shadow rounded-2">
+                <div>
+                    <h1 className='m-1'>Gaming rooms</h1>
+                    <ul class="list-group list-group-flush">
+                        {gamingRoomsInfo.map(item=>{
+                            return <li class="list-group-item bg-transparent" key={item.id}><Link to={`/gamingRooms/${item.id}/${item.name}/${type}`} className="text-decoration-none text-dark mya">{item.name}</Link></li>
+                        })}
+                    </ul>
+                </div>
+                <Link to='/' className="btn btn-dark btn-sm m-2">Home page</Link>
+            </div>
+        </div>
+        }   
+    </div> 
 }
