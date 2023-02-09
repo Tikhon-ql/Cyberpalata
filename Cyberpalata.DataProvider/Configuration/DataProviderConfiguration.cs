321ffa4b044e6d72a3f32c2b84a4265267cdc91d
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-02-10 00:10:59 +0300

    Repositories DI refactoring

--- Cyberpalata.DataProvider/Configuration/DataProviderConfiguration.cs
+++ Cyberpalata.DataProvider/Configuration/DataProviderConfiguration.cs
@@ -1,13 +1,10 @@
 ﻿using Cyberpalata.Common.Intefaces;
 using Cyberpalata.DataProvider.Context;
-using Cyberpalata.DataProvider.Context.Triggers;
 using Cyberpalata.DataProvider.Interfaces;
 using Cyberpalata.DataProvider.Repositories;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Cyberpalata.DataProvider.Configuration
 {
@@ -15,10 +12,6 @@ namespace Cyberpalata.DataProvider.Configuration
     {
         public static void ConfigureDataProvider(this IServiceCollection services, IConfiguration configuration)
         {
-            //. options.UseSqlServer(connection, b => b.MigrationsAssembly("Cyberpalata.WebApi")). By default,
-            //the migrations assembly is the assembly containing the DbContext.
-            //Change your target project to the migrations project by using the Package Mana
-
             var connectionString = configuration.GetConnectionString("CyberpalataConnectionString");
 
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -32,7 +25,6 @@ namespace Cyberpalata.DataProvider.Configuration
             services.AddScoped<IApiUserRepository, ApiUserRepository>();
             services.AddScoped<IRoomRepository, RoomRepository>();
             services.AddScoped<IGameRepository, GameRepository>();
-            services.AddScoped<IPriceRepository, PriceRepository>();
             services.AddScoped<IApiUserRepository, ApiUserRepository>();
             services.AddScoped<IGameConsoleRepository, GameConsoleRepository>();
             services.AddScoped<IUserRefreshTokenRepository, UserRefreshTokenRepository>();
@@ -40,11 +32,6 @@ namespace Cyberpalata.DataProvider.Configuration
             services.AddScoped<IPeripheryRepository, PeripheryRepository>();
             services.AddScoped<IBookingRepository, BookingRepository>();
             services.AddScoped<ISeatRepository, SeatRepository>();
-            //services.AddTransient<IGameConsoleRoomRepository, GameConsoleRoomRepository>();
-            //services.AddTransient<ISeatRepository, SeatRepository>();
-            //services.AddTransient<IMenuItemRepository, MenuItemRepository>();
-            //services.AddTransient<IGamingRoomRepository, GamingRoomRepository>();
-            //services.AddTransient<ILoungeRepository, LoungeRepository>();
         }
     }
 }
