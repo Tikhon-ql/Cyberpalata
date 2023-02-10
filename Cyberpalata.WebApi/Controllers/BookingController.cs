ae06f0e8bcad1a6dec1af4db817b211274cc8dfe
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-02-10 09:38:46 +0300

    GetRoomFreeSeats deleting

--- Cyberpalata.WebApi/Controllers/BookingController.cs
+++ Cyberpalata.WebApi/Controllers/BookingController.cs
@@ -18,60 +18,15 @@ namespace Cyberpalata.WebApi.Controllers
     {
         private readonly IBookingService _bookingService;
         private readonly ISeatService _seatService;
-        private readonly IPriceService _priceService;
         private readonly IRoomService _roomService;
-        //private readonly IRoomService _roomService;
         private readonly ILogger<BookingController> _logger;
 
-        public BookingController(IBookingService bookingService, ISeatService seatService,IPriceService priceService, IUnitOfWork uinOfWork, IRoomService roomService, ILogger<BookingController> logger) : base(uinOfWork)
+        public BookingController(IBookingService bookingService, ISeatService seatService, IUnitOfWork uinOfWork, IRoomService roomService, ILogger<BookingController> logger) : base(uinOfWork)
         {
             _bookingService = bookingService;
             _seatService = seatService;
-            _priceService = priceService;
             _roomService = roomService;
             _logger = logger;
-        }
-
-        //[Authorize]
-        [HttpGet("seats")]
-        public async Task<IActionResult> GetRoomFreeSeats(Guid roomId)
-        {
-            if(!ModelState.IsValid)
-            {
-                return BadRequest($"Bad request: {ModelState.ToString()}");
-            }
-            //var room = await _roomService.ReadAsync(roomId);
-            //if(room.HasNoValue)
-            //    return BadRequest("Bad room id!");
-            //Maybe<List<SeatDto>> freeSeats = new List<SeatDto>();//await _roomService.GetRoomFreeSeats(roomId);
-            //// no metter to check is seats null, so probably Maybe isn't needed.
-            //var tarrifs = room.Value.Prices;
-
-            //var resultSeats = new List<Seat>();
-
-            ////или создать метод проверки места и здесь вызывать?
-            //foreach(var item in room.Value.Seats)
-            //{
-            //    if (freeSeats.Value.FirstOrDefault(i => i.Id == item.Id) == null)
-            //        resultSeats.Add(new Seat {Number = item.Number, IsFree = false });
-            //    else
-            //        resultSeats.Add(new Seat { Number = item.Number, IsFree = true });
-            //}
-
-            //var resultSeats = await _seatService.GetSeatsByRoomId(roomId);
-            //if (resultSeats.HasNoValue)
-               //return BadRequest("Something wrong with room id or its seats");
-            var tariffs = await _priceService.GetByRoomIdAsync(roomId);
-            if(tariffs.HasNoValue)
-                return BadRequest("Something wrong with room id or its tariffs");
-
-            var viewModel = new BookingAddingViewModel
-            {
-                //Seats = resultSeats.Value.Select(s =>  new SeatViewModel { Number = s.Number, IsFree = s.IsFree}).ToList(),
-                Tariffs = tariffs.Value.Select(t => new PriceViewModel(t.Hours, t.Cost)).ToList()
-            };
-
-            return Ok(viewModel);
         }
 
         [Authorize]
