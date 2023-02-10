4a977e0dfb43253025c8fa15c06709e5c3e783a3
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-02-10 09:33:16 +0300

    SeatType refactoring

--- Cyberpalata.ViewModel/Booking/Enum/SeatType.cs
+++ Cyberpalata.ViewModel/Booking/Enum/SeatType.cs
@@ -5,8 +5,7 @@ namespace Cyberpalata.ViewModel.Booking.Enum
     public class SeatType : Enumeration
     {
         public static SeatType Free = new SeatType(1, "Free");
-        public static SeatType IsTaken = new SeatType(2, "IsTaken");
-        public static SeatType UsersSeat = new SeatType(3, "UsersSeat");
+        public static SeatType UsersSeat = new SeatType(2, "UsersSeat");
         public SeatType(int id, string name) : base(id, name)
         {
         }
