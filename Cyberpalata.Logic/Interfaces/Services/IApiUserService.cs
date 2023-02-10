0106ca1b52b9d297b5535de21a93d6b0c29d7259
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-02-10 09:27:52 +0300

    Mail confirm method deleting

--- Cyberpalata.Logic/Interfaces/Services/IApiUserService.cs
+++ Cyberpalata.Logic/Interfaces/Services/IApiUserService.cs
@@ -12,7 +12,6 @@ namespace Cyberpalata.Logic.Interfaces.Services
         Task UpdateUserAsync(UserUpdateRequest request);
         Task PasswordRecoveryAsync([EmailAddress] string email);
         Task<Result> ResetPasswordAsync(PasswordResetRequest request);
-        Task<Result> MailConfirmAsync(string email);
         int SendCodeToMail(string email);
         Task<Result> DeleteAsync(string email);
         Task<Result> ActivateUser(string email);
