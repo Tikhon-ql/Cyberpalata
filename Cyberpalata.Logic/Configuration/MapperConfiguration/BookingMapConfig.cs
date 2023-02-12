2c74956eeee1c46f8defad1ba7f1b9484bcef4d3
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-02-12 22:49:15 +0300

    Booking mapping editing

--- Cyberpalata.Logic/Configuration/MapperConfiguration/BookingMapConfig.cs
+++ Cyberpalata.Logic/Configuration/MapperConfiguration/BookingMapConfig.cs
@@ -3,6 +3,7 @@ using Cyberpalata.Common;
 using Cyberpalata.DataProvider.Models;
 using Cyberpalata.Logic.Models.Booking;
 using Cyberpalata.Logic.Models.Seats;
+using Cyberpalata.ViewModel.Request.Booking;
 
 namespace Cyberpalata.Logic.Configuration.MapperConfiguration
 {
@@ -15,12 +16,13 @@ namespace Cyberpalata.Logic.Configuration.MapperConfiguration
             profile.CreateMap<Maybe<Booking>, Maybe<BookingDto>>();
             profile.CreateMap<PagedList<Booking>, PagedList<BookingDto>>();
 
-            profile.CreateMap<BookingCreateRequest, BookingDto>()
+            profile.CreateMap<BookingCreateViewModel, BookingDto>()
                 .ForMember(dst=>dst.Date, opt=>opt.MapFrom(src=>src.Date))
                 .ForMember(dst => dst.Begining, opt => opt.MapFrom(src => src.Begining))
                 .ForMember(dst => dst.HoursCount, opt => opt.MapFrom(src => src.HoursCount))
                 .ForMember(dst => dst.Price, opt => opt.MapFrom(src => src.Price))
-                .ForMember(dst => dst.Seats, opt => opt.MapFrom(src => src.Seats.Select(item => new SeatDto { Number = item, IsFree = false })));
+                //A.K.: IsFree hadrcoded to false? Seems strange
+                .ForMember(dst => dst.Seats, opt => opt.MapFrom(src => src.Seats.Select(item => new SeatDto { Number = item, IsFree = true })));
         }
     }
 }
