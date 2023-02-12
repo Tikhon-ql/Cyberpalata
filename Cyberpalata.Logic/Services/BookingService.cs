7493a0ff05c07886773ecd58f4d7168b348e5c48
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-02-12 22:54:27 +0300

    Booking adding to room moving from data provider layer to business logic

--- Cyberpalata.Logic/Services/BookingService.cs
+++ Cyberpalata.Logic/Services/BookingService.cs
@@ -28,10 +28,52 @@ namespace Cyberpalata.Logic.Services
             return _mapper.Map<Maybe<BookingDto>>(booking);
         }
 
-        public async Task<PagedList<BookingDto>> GetPagedListAsync(int pageNumber, Guid userId)
+        public async Task<PagedList<BookingDto>> GetPagedListAsync(BaseFilter filter)
         {
-            var list = await _repository.GetPagedListAsync(pageNumber, userId);
+            var list = await _repository.GetPageListAsync(filter);
             return _mapper.Map<PagedList<BookingDto>>(list);
+        }
+        //???Is it an exception
+        public async Task<Result<BookingDetailsViewModel>> GetBookingDetail(Guid id)
+        {
+            var booking = await _repository.ReadAsync(id);
+            if (booking.HasNoValue)
+                return Result.Failure<BookingDetailsViewModel>($"Booking with id: {id} doen't exist!");
+            var viewModel = new BookingDetailsViewModel
+            {
+                RoomName = booking.Value.Room.Name,
+                Date = booking.Value.Date.ToString(),
+                Begining = booking.Value.Begining.ToString(@"hh\:mm"),
+                Price = booking.Value.Price,
+                HoursCount = booking.Value.HoursCount,
+                Seats = new List<SeatViewModel>(),
+            };
+            var seats = await _seatService.GetSeatsByRoomIdAsync(booking.Value.Room.Id);
+            var resultSeats = new List<SeatViewModel>();
+            seats.Value.ForEach((seat =>
+            {
+                resultSeats.Add(new SeatViewModel
+                {
+                    Number = seat.Number,
+                    Type = SeatType.Free,
+                });
+            }));
+            //resultSeats = resultSeats.OrderBy(seat => seat.Number).ToList();
+            //foreach (var bookingSeat in booking.Value.Seats)
+            //{
+            //    resultSeats[bookingSeat.Number - 1].Type = SeatType.UsersSeat;
+            //}
+            SetUsersSeats(resultSeats, booking.Value.Seats);
+            viewModel.Seats = resultSeats;
+            return Result.Success(viewModel);
+        }
+        private void SetUsersSeats(List<SeatViewModel> resultSeats, List<Seat> bookingsSeats)
+        {
+            resultSeats = resultSeats.OrderBy(seat => seat.Number).ToList();
+            foreach (var bookingSeat in bookingsSeats)
+            {
+                resultSeats[bookingSeat.Number - 1].Type = SeatType.UsersSeat;
+            }
         }
     }
 }
