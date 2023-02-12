c284661edf06b5ec4cbba67647bb04e8156fad3c
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-02-12 22:43:00 +0300

    PagedList filters adding

--- Cyberpalata.DataProvider/Interfaces/IRepository.cs
+++ Cyberpalata.DataProvider/Interfaces/IRepository.cs
@@ -1,13 +1,14 @@
 ﻿using CSharpFunctionalExtensions;
 using Cyberpalata.Common;
+using Cyberpalata.Common.Filters;
 
 namespace Cyberpalata.DataProvider.Interfaces
 {
     public interface IRepository<T> where T : class
     {
-        Task CreateAsync(T entity);
+        Task<Guid> CreateAsync(T entity);
         Task<Maybe<T>> ReadAsync(Guid id);
         void Delete(T entity);
-        Task<PagedList<T>> GetPageListAsync(int pageNumber);
+        Task<PagedList<T>> GetPageListAsync(BaseFilter filter);
     }
 }
