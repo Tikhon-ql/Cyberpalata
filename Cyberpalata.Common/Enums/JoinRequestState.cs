a89e493f15884f53813656f487a985d4ce9e6f56
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-03-06 16:14:42 +0300

    None state adding

--- Cyberpalata.Common/Enums/JoinRequestState.cs
+++ Cyberpalata.Common/Enums/JoinRequestState.cs
@@ -11,6 +11,7 @@ namespace Cyberpalata.Common.Enums
         public static JoinRequestState InProgress = new(1, "InProgress");
         public static JoinRequestState Accepted = new(2, "Accepted");
         public static JoinRequestState Rejected = new(3, "Rejected");
+        public static JoinRequestState None = new(4, "None");
 
         public JoinRequestState(int id, string name) : base(id, name)
         {
