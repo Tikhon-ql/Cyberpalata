fa16720227294c498bb3e018c5f6198210410683
DESKTOP-1BQJEED\User <tikhon.grek@mail.ru>
2023-03-06 16:20:54 +0300

    JoinRequestState adding

--- Cyberpalata.Logic/Models/Tournament/TeamJoinRequestDto.cs
+++ Cyberpalata.Logic/Models/Tournament/TeamJoinRequestDto.cs
@@ -1,10 +1,12 @@
-﻿using Cyberpalata.Logic.Models.Identity;
+﻿using Cyberpalata.Common.Enums;
+using Cyberpalata.Logic.Models.Identity;
 
 namespace Cyberpalata.Logic.Models.Tournament
 {
     public class TeamJoinRequestDto
     {
-        public Guid TeamId { get; set; }
+        public TeamDto Team { get; set; }
         public UserDto User { get; set; }
+        public JoinRequestState State { get; set; }
     }
 }
