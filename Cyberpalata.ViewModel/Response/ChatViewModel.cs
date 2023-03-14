fce557ce21b9a19bf9f44c3fba0490e0ea7c8b95
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 12:45:47 +0300

    Chat addingtional props adding

--- Cyberpalata.ViewModel/Response/ChatViewModel.cs
+++ Cyberpalata.ViewModel/Response/ChatViewModel.cs
@@ -9,7 +9,10 @@ namespace Cyberpalata.ViewModel.Response
     public class ChatViewModel
     {
         public Guid ChatId { get; set; }
+        public Guid OtherId { get; set; }
+        public Guid TeamId { get; set; }
         public string OtherUserName {  get; set; }
         public string OtherUserSurname { get; set; }
+        public bool IsYouACaptain {  get; set; }
     }
 }
