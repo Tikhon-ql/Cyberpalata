0b074909dcc487f4126dfe86ac49505d4f4d228e
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 09:53:14 +0300

    Team WinCount and approved statement props adding

--- Cyberpalata.Logic/Models/Tournament/TeamDto.cs
+++ Cyberpalata.Logic/Models/Tournament/TeamDto.cs
@@ -7,6 +7,8 @@
         public List<TeamMemberDto> Members { get; set; } = new();
         public List<TournamentDto> Tournaments { get; set; } = new();
         public bool IsHiring { get; set; }
+        public int WinCount { get; set; }
+        public bool IsApproved { get; set; }
         public TeamMemberDto Captain => Members.FirstOrDefault(m => m.IsCaptain);
     }
 }
