8c8716283bb8b3309af714e32334f18e773703b5
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 09:52:46 +0300

    Teams approved statement adding

--- Cyberpalata.Logic/Models/Tournament/BatleDto.cs
+++ Cyberpalata.Logic/Models/Tournament/BatleDto.cs
@@ -5,6 +5,8 @@
         public DateTime Date { get; set; }
         public TeamDto? FirstTeam { get; set; }
         public TeamDto? SecondTeam { get; set; }
+        public bool IsFirstTeamApproved { get; set; }
+        public bool IsSecondTeamApproved { get; set; }
         public int RoundNumber { get; set; }
     }
 }
