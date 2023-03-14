becec6313d57c109bd6af743770cd225258d2324
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 09:34:33 +0300

    SearchName filtration parametr adding

--- Cyberpalata.Logic/Filters/TournamentFilterBL.cs
+++ Cyberpalata.Logic/Filters/TournamentFilterBL.cs
@@ -4,6 +4,7 @@ namespace Cyberpalata.Logic.Filters
 {
     public class TournamentFilterBL : BaseFilterBL
     {
+        public Maybe<string> SearchName { get; set; }
         public Maybe<Guid> CaptainId { get;set; }
         public Maybe<bool> IsActual { get; set; }
     }
