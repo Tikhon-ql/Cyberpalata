5f17ce6343c155234b55d7e3e5d0bbbf6414876a
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 12:40:37 +0300

    Chat service logic

--- Cyberpalata.Logic/Services/ChatService.cs
+++ Cyberpalata.Logic/Services/ChatService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using CSharpFunctionalExtensions;
 using Cyberpalata.Common;
 using Cyberpalata.DataProvider.Filters;
 using Cyberpalata.DataProvider.Interfaces;
@@ -30,10 +31,24 @@ namespace Cyberpalata.Logic.Services
             await _chatRepository.CreateAsync(_mapper.Map<Chat>(chat));
         }
 
+        public async Task<Maybe<ChatDto>> ReadAsync(Guid chatId)
+        {
+            var chat = await _chatRepository.ReadAsync(chatId);
+            return _mapper.Map<Maybe<ChatDto>>(chat);
+        }
         public async Task<PagedList<ChatDto>> GetPagedList(ChatFilterBL filter)
         {
             var list = await _chatRepository.GetPageListAsync(_mapper.Map<ChatFilter>(filter));
             return _mapper.Map<PagedList<ChatDto>>(list);
+        }
+
+        public async Task<Result> SetIsDeletedState(Guid chatId ,bool isDeletedState)
+        {
+            var chat = await _chatRepository.ReadAsync(chatId);
+            if (chat.HasNoValue)
+                return Result.Failure($"Chat with id: {chatId} not found");
+            chat.Value.IsDeleted = isDeletedState;
+            return Result.Success();
         }
     }
 }
