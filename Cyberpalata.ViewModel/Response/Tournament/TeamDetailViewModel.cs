57e1f55ec0b6a5d4f282d16d77f230239745c14d
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 12:50:45 +0300

    Members adding

--- Cyberpalata.ViewModel/Response/Tournament/TeamDetailViewModel.cs
+++ Cyberpalata.ViewModel/Response/Tournament/TeamDetailViewModel.cs
@@ -2,8 +2,9 @@
 {
     public class TeamDetailViewModel
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string CaptainName { get; set; }
-        public List<string> Members { get; set; }
+        public List<TeamMemberViewModel> Members { get; set; }
     }
 }
