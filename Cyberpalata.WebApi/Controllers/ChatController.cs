00c5ddb904956208d0ae29ba6c43c5e0e49f864c
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 12:54:40 +0300

    Message getting endpoint

--- Cyberpalata.WebApi/Controllers/ChatController.cs
+++ Cyberpalata.WebApi/Controllers/ChatController.cs
@@ -40,8 +40,11 @@ namespace Cyberpalata.WebApi.Controllers
                 viewModel.Add(new ChatViewModel
                 {
                     ChatId = chat.Id,
+                    OtherId = chat.Captain.Id == userId ? chat.UserToJoin.Id : chat.Captain.Id,
                     OtherUserName = chat.Captain.Id == userId ? chat.UserToJoin.Username : chat.Captain.Username,
                     OtherUserSurname = chat.Captain.Id == userId ? chat.UserToJoin.Surname : chat.Captain.Surname,
+                    TeamId = chat.Captain.Team != null ? chat.Captain.Team.Id : Guid.Empty,
+                    IsYouACaptain = chat.Captain != null ? chat.Captain.Id == userId : false,
                 });
             }
             return Ok(new { PageSize = chatsResult.PageSize, TotalItemsCount = chatsResult.TotalItemsCount, Items = viewModel });
@@ -49,19 +52,22 @@ namespace Cyberpalata.WebApi.Controllers
 
         [Authorize]
         [HttpGet("getMessages")]
-        public async Task<IActionResult> GetMessages()
+        public async Task<IActionResult> GetMessages(Guid chatId)
         {
-            var userId = Guid.Parse(User.Claims.First(claim=>claim.Type == JwtRegisteredClaimNames.Sid).Value);
-
-            return null;
-            //var filter = new MessageFilterBL
-            //{
-            //    CurrentPage = 1,
-            //    PageSize = int.MaxValue,
-            //    UserId = userId
-            //};
-            //var result = await _messageService.GetPagedList(filter);
-            //return Ok(result);
+            var chat = await _chatService.ReadAsync(chatId);
+            if (chat.HasNoValue)
+                return BadRequest();
+            var viewModel = new List<MessageViewModel>();
+            foreach (var message in chat.Value.Messages.OrderBy(message=>message.SentDate))
+            {
+                viewModel.Add(new MessageViewModel
+                {
+                    User = message.Sender.Email,
+                    Message = message.MessageText,
+                    SentDate = message.SentDate.ToString("f")
+                });
+            }
+            return Ok(viewModel);
         }
     }
 }
