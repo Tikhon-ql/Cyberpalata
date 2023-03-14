8068a7c49aed5d603e96f866d57c552ca8d48ed4
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 09:42:09 +0300

    Room filter enriching

--- Cyberpalata.DataProvider/Filters/RoomFilter.cs
+++ Cyberpalata.DataProvider/Filters/RoomFilter.cs
@@ -6,16 +6,53 @@ namespace Cyberpalata.DataProvider.Filters
 {
     public class RoomFilter : BaseFilter<Room>
     {
-        public Maybe<RoomType> Type { get; set; }
-        public Maybe<bool> IsVip { get; set; }
+        public Maybe<string> SearchName { get; set; }
+        public Maybe<int> FreeSeatsCount { get; set; }
+        public Maybe<int> FreeSeatsInRowCount { get; set; }
+        public Maybe<DateTime> Date { get; set; }
+        public Maybe<int> HoursCount { get; set; }
+        public Maybe<TimeSpan> Begining { get; set; }
 
         public override IQueryable<Room> EnrichQuery(IQueryable<Room> query)
         {
-            if (Type.HasValue)
-                query = query.Where(q => q.Type == Type.Value);
-            if(IsVip.HasValue)
-                query = query.Where(q=>q.IsVip == IsVip.Value);
+            if (SearchName.HasValue)
+                query = query.Where(q => q.Name.Contains(SearchName.Value));
+            if (FreeSeatsCount.HasValue && Date.HasValue && HoursCount.HasValue && Begining.HasValue)
+            {
+                query = query.ToList().Where(q => HasMoreFreeSeatsThan(FreeSeatsCount.Value, q) == true).AsQueryable();
+            }
+            var list = query.ToList();
             return query;
+        }
+
+        private bool HasMoreFreeSeatsThan(int freeSeatsCount, Room room)
+        {
+            int freeSeats = 0;
+
+            var roomSeats = room.Seats;
+            if (roomSeats.Count == 0)
+                return false;
+
+            var actualRoomBookings = room.Bookings.Where(b => b.Date >= DateTime.UtcNow).ToList();
+
+            //TODO: PUSH IT INTO SEPARATE METHODE
+            var bookings = actualRoomBookings
+                .Where(b => (b.Date == Date.Value
+                && ((Begining.Value <= b.Begining
+                    && Date.Value.Add(Begining.Value).AddHours(HoursCount.Value) > b.Date.Add(b.Begining))
+                || (b.Begining <= Begining.Value
+                    && Date.Value.Add(Begining.Value) < b.Date.Add(b.Begining).AddHours(b.HoursCount))))).ToList();
+            foreach (var seat in roomSeats)
+            {
+                var isSeatFree = bookings.FirstOrDefault(b => b.Seats.FirstOrDefault(s => s.Id == seat.Id) != null) == null;
+                if (isSeatFree)
+                {
+                    freeSeats++;
+                }
+            }
+            if (freeSeats >= freeSeatsCount)
+                return true;
+            return false;
         }
     }
 }
