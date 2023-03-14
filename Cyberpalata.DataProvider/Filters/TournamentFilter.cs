83e4f1b1104f7a6184faaad8d013f84fa2803687
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 09:43:13 +0300

    Tournament filter Searching adding

--- Cyberpalata.DataProvider/Filters/TournamentFilter.cs
+++ Cyberpalata.DataProvider/Filters/TournamentFilter.cs
@@ -5,11 +5,14 @@ namespace Cyberpalata.DataProvider.Filters
 {
     public class TournamentFilter : BaseFilter<Tournament>
     {
+        public Maybe<string> SearchName { get; set; }
         public Maybe<Guid> CaptainId { get; set; }
         public Maybe<bool> IsActual { get; set; }
 
         public override IQueryable<Tournament> EnrichQuery(IQueryable<Tournament> query)
-        {   
+        {
+            if (SearchName.HasValue)
+                query = query.Where(q=>q.Name.Contains(SearchName.Value));
             if (CaptainId.HasValue)
             {
                 query = query.Where(q => q.Batles.Any(b => b.FirstTeam.Members.Any(m => m.IsCaptain && m.MemberId == CaptainId.Value) 
@@ -18,7 +21,7 @@ namespace Cyberpalata.DataProvider.Filters
             if (IsActual.HasValue)
             {
                 if(IsActual.Value)
-                    query = query.Where(q=>q.Date > DateTime.UtcNow);
+                    query = query.Where(q=>q.Date >= DateTime.UtcNow);
                 else
                     query = query.Where(q => q.Date < DateTime.UtcNow);
             }
