8fea252e9697af2b29c92c674409bf9f70416b46
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 09:42:24 +0300

    Team filter adding

--- Cyberpalata.DataProvider/Filters/TeamFilter.cs
+++ Cyberpalata.DataProvider/Filters/TeamFilter.cs
@@ -6,14 +6,28 @@ namespace Cyberpalata.DataProvider.Filters
     public class TeamFilter : BaseFilter<Team>
     {
         public Maybe<Guid> CaptainId { get; set; }
+        public Maybe<Guid> MemberId { get; set; }
         public Maybe<bool> IsHiring { get; set; }
+        public Maybe<bool> IsTop { get; set; }
 
         public override IQueryable<Team> EnrichQuery(IQueryable<Team> query)
         {
             if (CaptainId.HasValue && CaptainId.Value != Guid.Empty)
                 query = query.ToList().Where(q => q.Members.FirstOrDefault(m => m.IsCaptain && m.MemberId == CaptainId.Value) != null).ToList().AsQueryable();
+            if(MemberId.HasValue && CaptainId.HasNoValue)
+            {
+                query = query.Where(q =>
+                    q.Members.FirstOrDefault(m => m.Member.Id == MemberId.Value) != null);
+            }
             if (IsHiring.HasValue)
                 query = query.Where(q => q.IsHiring == IsHiring.Value);
+            if(IsTop.HasValue)
+            {
+                if(IsTop.Value)
+                {
+                    query = query.OrderByDescending(query=>query.WinCount);
+                }
+            }
             return query.ToList().AsQueryable();
         }
     }
