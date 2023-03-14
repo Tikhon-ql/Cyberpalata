e134ee1d1e32b17d07956c92471b3e6f7e3f604f
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 09:51:24 +0300

    Create method editing

--- Cyberpalata.Logic/Interfaces/Services/ITeamService.cs
+++ Cyberpalata.Logic/Interfaces/Services/ITeamService.cs
@@ -9,10 +9,11 @@ namespace Cyberpalata.Logic.Interfaces.Services
 {
     public interface ITeamService
     {
-        Task CreateAsync(CreateTeamViewModel request);
+        Task<Result> CreateAsync(CreateTeamViewModel request);
         Task<PagedList<TeamDto>> GetPagedList(TeamFilterBL filter);
         Task<Maybe<TeamDetailViewModel>> GetTeamDetailed(Guid teamId);
         Task SetHiringState(Guid teamId,bool state);
         Task<Result<TeamDetailViewModel>> GetTeamInTournament(Guid teamId, Guid tournamentId);
+        Task<Result> AddUserToTeam(Guid userId, Guid teamId);
     }
 }
