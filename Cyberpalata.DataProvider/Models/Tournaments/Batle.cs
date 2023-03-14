dd18e7b5aa825e9e63a096503fc10f4d42330f87
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 09:45:36 +0300

    Approving system adding

--- Cyberpalata.DataProvider/Models/Tournaments/Batle.cs
+++ Cyberpalata.DataProvider/Models/Tournaments/Batle.cs
@@ -5,7 +5,10 @@
         public DateTime Date { get; set; }
         public virtual Team? FirstTeam { get; set; }
         public virtual Team? SecondTeam { get; set;}
+        public bool IsFirstTeamApproved { get; set; }
+        public bool IsSecondTeamApproved { get; set; }
         public virtual Tournament Tournament { get;set; }
         public int RoundNumber { get; set; }
+        public int Number { get; set; }
     }
 }
