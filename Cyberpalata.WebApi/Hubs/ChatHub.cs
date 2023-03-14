205b11049ac6ddf0206a42b399002e99a62b4eb2
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 12:58:54 +0300

    Chat hub for signalR messaging

--- Cyberpalata.WebApi/Hubs/ChatHub.cs
+++ Cyberpalata.WebApi/Hubs/ChatHub.cs
@@ -1,7 +1,9 @@
-﻿using Cyberpalata.DataProvider.Models;
+﻿using Cyberpalata.Common.Intefaces;
+using Cyberpalata.DataProvider.Models;
 using Cyberpalata.DataProvider.Models.Identity;
 using Cyberpalata.Logic.Interfaces.Services;
 using Cyberpalata.ViewModel.Request;
+using Cyberpalata.WebApi.Connections;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
@@ -10,50 +12,48 @@ namespace Cyberpalata.WebApi.Hubs
     public class ChatHub : Hub
     {
         private readonly string _botUser;
-        private readonly IDictionary<string, UserConnection> _connections;
+        private readonly IDictionary<string, ChatConnection> _connections;
         private readonly IMessageService _messageService;
+        private readonly ILogger<ChatHub> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public ChatHub(IDictionary<string, UserConnection> connections, IMessageService messageService)
+        public ChatHub(IDictionary<string, ChatConnection> connections, IMessageService messageService, ILogger<ChatHub> logger, IUnitOfWork unitOfWork)
         {
             _botUser = "Chat bot";
             _connections = connections;
             _messageService = messageService;
+            _logger = logger;
+            _unitOfWork = unitOfWork;
         }
         //[Authorize]
         public override Task OnDisconnectedAsync(Exception? exception)
         {
-            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection connection))
-            {
+            if (_connections.TryGetValue(Context.ConnectionId, out ChatConnection connection))
+            {               
                 _connections.Remove(Context.ConnectionId);
-
-                Clients.Group(connection.ChatId.ToString()).SendAsync("ReceiveMessage", _botUser, $"{connection.User.Email} has left chat");
             }
             return base.OnDisconnectedAsync(exception);
         }
         //[Authorize]
-        public async Task SendMessage(string message)
+        public async Task SendMessage(MessageViewModel viewModel)
         {
-            if(_connections.TryGetValue(Context.ConnectionId, out UserConnection connection))
+            if(_connections.TryGetValue(Context.ConnectionId, out ChatConnection connection))
             {
-                var viewModel = new MessageViewModel
-                {
-                    ChatId = connection.ChatId,
-                    Sender = connection.User.Email,
-                    MessageText = message
-                };
+                _logger.LogCritical(viewModel.ChatId.ToString());
+                _logger.LogCritical(viewModel.Sender);
+                _logger.LogCritical(connection.User.Email);
+                _logger.LogCritical(viewModel.Message);
+               
                 await _messageService.CreateAsync(viewModel);
-                await Clients.Group(connection.ChatId.ToString()).SendAsync("ReceiveMessage",$"{connection.User.Email}",message);
+                await Clients.Group(connection.ChatId.ToString()).SendAsync("ReceiveMessage", $"{connection.User.Email}", viewModel.Message);
+                await _unitOfWork.CommitAsync();
             }
         }
         //[Authorize]
-        public async Task JoinRoom(UserConnection connection)
-        {
-            
+        public async Task JoinRoom(ChatConnection connection)
+        {       
             await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatId.ToString());
-
             _connections[Context.ConnectionId] = connection;
-
-            await Clients.Group(connection.ChatId.ToString()).SendAsync("ReceiveMessage",_botUser,$"{connection.User.Email} has joined chat");
         }
     }
 }
