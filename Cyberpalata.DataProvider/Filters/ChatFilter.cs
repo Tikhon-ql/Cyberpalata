a53d33aa684b660f26397039903604e4ae9d5ce9
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 09:32:14 +0300

    Getting onlu actual chats

--- Cyberpalata.DataProvider/Filters/ChatFilter.cs
+++ Cyberpalata.DataProvider/Filters/ChatFilter.cs
@@ -13,6 +13,7 @@ namespace Cyberpalata.DataProvider.Filters
         public Maybe<Guid> UserId { get;set; }
         public override IQueryable<Chat> EnrichQuery(IQueryable<Chat> query)
         {
+            query = query.Where(q => !q.IsDeleted);
             if (UserId.HasValue)
                 query = query.Where(q => q.Captain.Id == UserId.Value || q.UserToJoin.Id == UserId.Value);
             return query;
