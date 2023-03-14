0616446373a15d2b2d66c7225ae9b121c4685513
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 12:50:31 +0300

    Aditional props adding

--- Cyberpalata.ViewModel/Response/Tournament/GetTournamentViewModel.cs
+++ Cyberpalata.ViewModel/Response/Tournament/GetTournamentViewModel.cs
@@ -4,5 +4,9 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public int TeamsCount { get; set; }
+        public string Date { get; set; }
+        public string Begining { get; set; }
+        public bool IsCaptain { get; set; }
     }
 }
