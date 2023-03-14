92fe3dabc362442f108b95cad7a8b6a78130cad1
WIN-4FP6ODQRSBT\user <tikhon.grek@mail.ru>
2023-03-14 09:34:12 +0300

    MemberId and IsTop filtration parametrs adding

--- Cyberpalata.Logic/Filters/TeamFilterBL.cs
+++ Cyberpalata.Logic/Filters/TeamFilterBL.cs
@@ -5,6 +5,8 @@ namespace Cyberpalata.Logic.Filters
     public class TeamFilterBL : BaseFilterBL
     {
         public Maybe<Guid> CaptainId { get; set; }
+        public Maybe<Guid> MemberId { get; set; }
         public Maybe<bool> IsHiring { get; set; }
+        public Maybe<bool> IsTop { get; set; }
     }
 }
